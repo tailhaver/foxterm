bbc08979caf9a51838f7077799a95c3c4f4b156d
Taggie <taggie@yip.cat>
2025-09-13 06:26:33 -0400

    fix blueprints/__init__.py

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -1,2 +1,2 @@
-from index import blueprint as index_blueprint
-from term import blueprint as term_blueprint
+from .index import blueprint as index_blueprint
+from .term import blueprint as term_blueprint
